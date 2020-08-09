8178d726e7a594d1ba57c68ddd17420312106a91
Brian Kamras <bkamras09@gmail.com>
2020-08-08 22:23:16 -0700

    new indent stuff

--- indent/main.c
+++ indent/main.c
@@ -1,25 +1,42 @@
 /* OVERVIEW: indents an incoming text stream and puts the resulting text stream back into the same place it was in the source file. */
 
 #include <stdio.h>
+#include <string.h>
 
-#define BUF		1024 /* max size allowed for the input stream */
 #define MAX		1000
 
-void get_input(char *s, char *line_pointer[]);
-void indent(char *input);
+void get_input(char *in);
+void indent(char *in);
 
-int main (void){
+int main (void) {
+	char input[MAX];
 	printf("Formatting incoming text stream\n");
+	get_input(input);
+	/*indent(input); */
+	printf("%c\n", input);
 }
 
-void get_input(char *s, char *line_pointer[]) {
-  int c;
-  char *ps = s;
-  for (; *ps < MAX-1 && (c = getchar()) != EOF && c != '\n'; ps++)
+void get_input(char *s) {
+	int c;
+	char *ps = s;
+	for (; *ps < MAX-1 && (c = getchar()) != EOF; ps++) {
 	 *ps = c;
-  *ps = '\0';
+	}
+	*ps = '\0';
 }
 
-void indent(char *input) {
-	int i = 0;
+void indent(char *in) {
+	char *inp = in;
+	char out[MAX - 1];
+	char *outp = out;
+	for (char *inp = in; *inp < strlen(in); inp++) {
+		if (*inp != '\n') {
+			*outp = *inp;
+			printf("out", "%c\n", out);
+			outp++;
+		} else {
+			*outp = '\t';
+			outp++;
+		}
+	}
 }

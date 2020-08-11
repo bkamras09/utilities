9943f5f885ff59bde028aefc3228dea9df8279e2
Brian Kamras <brian.kamras@invene.com>
2020-08-11 15:14:40 -0500

    more outdent

--- indent/main.c
+++ indent/main.c
@@ -2,25 +2,31 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <X11/Xlib.h>
+/* #include <X11/Xlib.h> */
 
 #define MAX		1000 /* maximum allowed array length to indent */
 
 char * get_input(char *input);
 char * scan_input(char *input);
 char * indent(char *input, char *output);
+char * outdent(char *input, char *output);
 
 int main (void) {
 	char input[MAX];
 	char output[MAX];
+	char outdented_output[MAX];
 	get_input(input);
+	/* scanf("%s", &input[MAX]); */
 	indent(input, output);
-	printf("output:\n%s\n", output);
+	outdent(input, outdented_output);
+	printf("\nindented output:\n%s\n", output);
+	printf("outdented output: \n%s\n", outdented_output);
+	
 }
 
 /* get input from stdin and return a pointer to the input array */
 char * get_input(char *s) {
-	int c;
+	unsigned long int c;
 	char *ps = s;
 
 	for (; *ps < MAX - 1 && (c = getchar()) != EOF; ps++)
@@ -29,10 +35,7 @@ char * get_input(char *s) {
 	return s;
 }
 
-char * scan_input(char *s){
-}
-
-/* TODO: make indent increment by length of each array element using sizeof() */
+/* TODO: make indent increment by length of each array element */
 char * indent(char *in, char *out) {
 	char *inp = in;
 	char *outp = out;
@@ -51,14 +54,18 @@ char * indent(char *in, char *out) {
 	return out;
 }
 
+/* TODO: make outdent increment by length of each array element */
+/* TODO: make outdent only remove \t after a \n */
 char * outdent(char *in, char *out){
 	char *inp = in;
 	char *outp = out;
 
 	for (; *inp != '\0'; outp++, inp++){
-		if (*inp == '\t'){
+		if (*inp != '\t' && inp-- && *inp == '\n'){
 			inp++;
+			*outp = *inp;
 		} else {
+			inp++;
 			*outp = *inp;
 		}
 	}

ebcba387ad12c171805623be7cd6f21d0155d3b9
Brian Kamras <brian.kamras@invene.com>
2020-08-19 00:05:41 -0500

    more updates to file handling

--- ted/main.c
+++ ted/main.c
@@ -3,27 +3,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 /* #include <X11/Xlib.h> */
 
 #define MAX		1000 /* maximum allowed array length to indent */
 #define LENGTH	255 /* maximum allowed file name length */
+#define IN 2
+#define OUT 3
+#define NS from[i] != '\0'
 
 char * get_input(char *input);
 char * scan_input(char *input);
 char * indent(char *input, char *output);
 char * outdent(char *input, char *output);
+void uncom(char to[], char from[]);
 void filecopy(FILE *ifp, FILE *ofp);
 
 int main (int argc, char *argv[]){
 	FILE *input_file;
 	char input[MAX];
 	if (argc > 1){
-		input_file = fopen(argv[1], "r+");
+		printf("Welcome to ted, the tiny text editor.\n");
+		if (access(argv[1], F_OK) != -1){
+			printf("File exists.  Opening it now...\n");
+			input_file = fopen(argv[1], "r+");
+		} else {
+			printf("File does not exist.  Creating it now...\n");
+			input_file = fopen(argv[1], "w+");
+		}
 	} else{
 		printf("usage: ted filename\n");
 		return 0;
 	}
-	printf("Welcome to ted, the tiny text editor.\n");
 	int c;
     while ((c = getc(input_file)) != EOF){
         putchar(c);
@@ -83,6 +94,50 @@ char * outdent(char *in, char *out){
 	return out;
 }
 
+void uncom(char to[], char from[])
+{
+	int state, i, j;
+	state = OUT;
+	i = j = 0;
+
+	while (state == OUT && NS) {
+		if (from[i] == '/') {
+			to[j] = from[i];
+			++i;
+			++j;
+			if (from[i] == '*') {
+				++i;
+				--j;
+				state = IN;
+				while (state == IN && NS) {
+				if (from[i] == '*') {
+					++i;
+					if (from[i] == '/') {
+						state = OUT;
+						++i;
+					}
+					else
+						++i;
+				}
+				else
+					++i;
+				}
+			}
+			else {
+				to[j] = from[i];
+				++i;
+				++j;
+			}
+		}
+		else {
+			to[j] = from[i];
+			++i;
+			++j;
+		}
+	}
+	to[j] = '\0';
+}
+
 /* random function from 4chan that I need to test */
 /*
 char *indent(char *in)
@@ -99,4 +154,4 @@ char *indent(char *in)
     memcpy(q, p1, strlen(p1) + 1);
     return out;
 }
-*//* This is a silly addition@ */
+*/
